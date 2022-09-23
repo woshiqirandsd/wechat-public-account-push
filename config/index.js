ae36a7f1fb4563360fca5703165e61102c59ada1
woshiqirandsd <114203267+woshiqirandsd@users.noreply.github.com>
2022-09-23 14:08:21 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -60,7 +60,7 @@ export const USER_CONFIG = {
   USERS: [,
     {
       // 想要发送的人的名字
-      name: "老婆0",
+      name: "老婆  早上好",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oxU5s5tBeWc7qW8rFKxL6wIk8uPE",
       // 你想对他发送的模板消息的模板ID
