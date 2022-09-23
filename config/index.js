d5f9716e8d12f6dc3e404751ded0a20692fd90f1
woshiqirandsd <114203267+woshiqirandsd@users.noreply.github.com>
2022-09-23 14:37:15 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ export const USER_CONFIG = {
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "04098684340e4c883e1b77ba49d027cc",
+  APP_SECRET: "ed3e192d1961874df7e0620ac929dcdf",
   
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -57,7 +57,7 @@ export const USER_CONFIG = {
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
    */
-  USERS: [ "gh_e205673927fe"]
+  USERS: [ 
     {
       // 想要发送的人的名字
       name: "老婆  早上好",
