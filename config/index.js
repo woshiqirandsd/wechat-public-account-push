2630477903bbddd453463d2ea4d80f3261fc61f0
woshiqirandsd <114203267+woshiqirandsd@users.noreply.github.com>
2022-09-23 11:32:08 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -12,7 +12,6 @@ export const USER_CONFIG = {
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
   APP_SECRET: "04098684340e4c883e1b77ba49d027cc",
-  TEMPLATE_ID:"7LiE7k1CiogA7zN5ZiwwQiy6ZfYj4PGASv6WEjl8kCo",
   
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -139,7 +138,8 @@ export const USER_CONFIG = {
    */
   
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "",
+  CALLBACK_TEMPLATE_ID: "7LiE7k1CiogA7zN5ZiwwQiy6ZfYj4PGASv6WEjl8kCo
+",
   
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
